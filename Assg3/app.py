38acbf45633255e7f1be269092303fef1bc6a4dd
Łukasz <247660@edu.p.lodz.pl>
2025-01-08 13:34:06 +0100

    - naprawilem predict i dodalem ladowanie danych z pliku, usunalem sigma z jednej strony i dodalem rounda do 4 miejsc po przecinku

--- Assg3/app.py
+++ Assg3/app.py
@@ -33,7 +33,37 @@ def create_app():
         try:
             db.create_all()
             db.session.commit()
+
+            load_data_from_file('data/seeds.txt')
+
             apply_migrations()
         except Exception as e:
             print(e)
     return app
+
+
+def load_data_from_file(file_path):
+    try:
+        from models import Seed
+        if not Seed.query.first():
+            print("Database is empty. Loading data")
+
+            with open(file_path, 'r') as file:
+                for line in file:
+                    fields = line.strip().split(',')
+                    seed = Seed(
+                        area=round(float(fields[0]),4),
+                        perimeter=round(float(fields[1]),4),
+                        compactness=round(float(fields[2]),4),
+                        kernel_length=round(float(fields[3]),4),
+                        kernel_width=round(float(fields[4]),4),
+                        asymmetry_coefficient=round(float(fields[5]),4),
+                        kernel_groove_length=round(float(fields[6]),4),
+                        seed_class=int(fields[7])
+                    )
+                    db.session.add(seed)
+                db.session.commit()
+                print("Data loaded successfully.")
+    except Exception as e:
+        db.session.rollback()
+        print(f"Failed to load data: {e}")
--- Assg3/models.py
+++ Assg3/models.py
@@ -1,6 +1,8 @@
 from app import db
 
+
 class Seed(db.Model):
+
     __tablename__ = 'seeds'
 
     seed_id = db.Column(db.Integer, primary_key=True)
